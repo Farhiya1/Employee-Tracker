37c4c6aba9cfb02d5e1366ecad9f9914f98cde42
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-19 00:32:29 +0000

    Connection to mysql2 server

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ const inquirer = require("inquirer");
 var mysql2 = require("mysql2");
 const consoleTable = require("console.table");
 
+// Create connection to mysql
+var connection = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "",
+  database: "employee_db",
+});
+
+// Connect to the server
+connection.connect((err) => {
+  if (err) {
+    // If there's an error return Error.
+    console.log("Error!!", err);
+  } else {
+    // You are Connected
+    console.log("You are connected to the MySQL Server");
+    userOptions();
+  }
+});
+
