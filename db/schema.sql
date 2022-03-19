9439182fe1e5eb4ee96fb13bf2defe274b73d28e
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-19 00:27:01 +0000

    updated schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,5 @@
 DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db;
 
 USE employee_db;
@@ -11,5 +12,19 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_ID INT NOT NULL
     
-)
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER
+);
+
+SELECT * FROM department
+SELECT * FROM employee
+SELECT * FROM role
