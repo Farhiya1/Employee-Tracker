12e065d8a82487a3b2ae7d6e836a16010bd5b979
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-25 22:20:59 +0000

    function add department added

--- app.js
+++ app.js
@@ -101,4 +101,29 @@ function viewRoles() {
 
     userOptions();
   });
+}
+
+// add a department to the database
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "newDepartment",
+        type: "input",
+        message: "Which department would you like to add?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query("INSERT INTO department SET ?", {
+        name: answer.newDepartment,
+      });
+      var query = "SELECT * FROM department";
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.log("Your department has been added!");
+        console.table("All Departments:", res);
+        userOptions();
+      });
+    });
+}
 
