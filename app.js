4f555af91ca6c637209b8b16b4aae6e4ab12fa9e
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-25 22:21:25 +0000

    add employee function added

--- app.js
+++ app.js
@@ -127,3 +127,62 @@ function addDepartment() {
     });
 }
 
+function addEmployee() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          type: "input",
+          message: "What is the employee's first name? ",
+        },
+        {
+          name: "last_name",
+          type: "input",
+          message: "What is the employee's last name? ",
+        },
+        {
+          name: "manager_id",
+          type: "input",
+          message: "What is the employee's manager's ID? ",
+        },
+        {
+          name: "role",
+          type: "list",
+          choices: function () {
+            var roleArray = [];
+            for (let i = 0; i < res.length; i++) {
+              roleArray.push(res[i].title);
+            }
+            return roleArray;
+          },
+          message: "What is this employee's role? ",
+        },
+      ])
+      .then(function (answer) {
+        let role_id;
+        for (let x = 0; x < res.length; x++) {
+          if (res[x].title == answer.role) {
+            role_id = res[x].id;
+            console.log(role_id);
+          }
+        }
+        connection.query(
+          "INSERT INTO employee SET ?",
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            manager_id: answer.manager_id,
+            role_id: role_id,
+          },
+          function (err) {
+            if (err) throw err;
+            console.log("You have successfully added your employee!");
+            userOptions();
+          }
+        );
+      });
+  });
+}
+
